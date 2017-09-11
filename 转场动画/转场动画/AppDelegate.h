7c0d09a02e798f0bc2bbfa81ce79ad379aaf86d2
aizhaoliang <aizhaoliang1993@gmail.com>
2017-09-11 10:10:24 +0800

    添加转场文件

---
+++ 转场动画/转场动画/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  转场动画
+//
+//  Created by ai on 2017/9/11.
+//  Copyright © 2017年 ai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
